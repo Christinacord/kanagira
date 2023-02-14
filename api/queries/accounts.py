4bdfe1d4da33fd733247ce7389b864997d976c7d
Joe Biedermann <jbiedz13@gmail.com>
2023-02-14 16:12:35 -0600

    routers and queries

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -25,7 +25,7 @@ class AccountOutWithPassword(AccountOut):
     hashed_password: str
 
 class AccountQueries(Queries):
-    # region properties-
+    
 
     def get(self, email: str) -> AccountOutWithPassword:pass
     def create(self, info: AccountIn, hashed_password: str) -> AccountOutWithPassword:pass
