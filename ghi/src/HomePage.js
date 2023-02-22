39ae1159987bd35007cfb5424d0c2693611219aa
Joe Biedermann <jbiedz13@gmail.com>
2023-02-22 17:29:13 -0600

    added HomePage, footer, edited apps.js

---
+++ ghi/src/HomePage.js
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+
+
+function HomePage() {
+    return (
+        <>
+            <div>
+                <h1>Concept to Launch in Record Time</h1>
+                <img src="https://images.unsplash.com/photo-1636819488524-1f019c4e1c44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"></img>
+                <div>
+                    <h3>Features:</h3>
+                    <ul>
+                        <li>100% free!</li>
+                        <li>Create team boards</li>
+                        <li>Track tasks from start to finish</li>
+                        <li>Easily manage workflow</li>
+                        <li>Deliver better business outcomes</li>
+                    </ul>
+                </div>
+                <h1>"Don't trust your memory, trust Kanagira!."</h1>
+            </div>
+            <div>
+            </div>
+        </>
+    );
+}
+
+export default HomePage;
