fdde905806f0628e31ce6200036222c040c87636
Christina Cord <christinacord@gmail.com>
2023-02-23 22:56:34 +0000

    Completed front-end authentication.

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,15 +5,27 @@ import { useEffect, useState } from "react";
 import Construct from "./Construct.js";
 import ErrorNotification from "./ErrorNotification";
 import "./App.css";
+import { AuthProvider, useToken } from "./auth.js";
+import LoginComponent from "./LoginForm";
+
+function GetToken() {
+  // Get token from JWT cookie (if already logged in)
+  useToken();
+  return null;
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <GetToken />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginComponent />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
