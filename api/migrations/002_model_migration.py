4c46ba36863c887101a75d6a1e9475eb5cb90fe5
Ryan <ryan2paschen@gmail.com>
2023-02-24 12:06:51 -0500

    Fixed formatting issues

--- api/migrations/002_model_migration.py
+++ api/migrations/002_model_migration.py
@@ -37,7 +37,9 @@ steps = [
             type VARCHAR(100) NOT NULL,
             difficulty INTEGER NOT NULL,
             creator_id INTEGER REFERENCES accounts("id") ON DELETE CASCADE,
-            assignee_id INTEGER REFERENCES accounts("id") ON DELETE CASCADE NULL,
+            assignee_id INTEGER REFERENCES accounts("id")
+                ON DELETE CASCADE
+                NULL,
             swim_lane_id INTEGER REFERENCES swim_lanes("id") ON DELETE CASCADE
         );
         """,
