968bfb96a863ca6df3f68f57a5edab59907711dd
Daniel Im <danielsungunim@gmail.com>
2023-03-03 20:39:46 -0500

    merge conflict resolution

--- ghi/src/Test.js
+++ ghi/src/Test.js
@@ -1,10 +1,170 @@
 import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import { useToken } from "./auth.js";
+import { useNavigate } from "react-router-dom";
 
 export default function MyApp() {
-    return (
-        <div>
-            <Button variant="contained">Hello World</Button>
-        </div>
-    );
+    const navigate = useNavigate();
+    const { token, logout } = useToken();
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        logout();
+        navigate("/");
+    };
+
+    if (token) {
+        return (
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <RocketLaunchIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography variant="h6" noWrap component="a" href="/" sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
+                            KANAGIRA
+                        </Typography>
+                        <Box id="mobile-responsive-hamburger-menu" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left', }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' }, }}>
+                                <MenuItem id="menu-appbar" component="a" href="#" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">About</Typography>
+                                </MenuItem>
+                                <MenuItem id="menu-appbar" component="a" href="#" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">Contact Us</Typography>
+                                </MenuItem>
+                                <MenuItem id="menu-appbar" component="a" href="#" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">FAQ</Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+                        <RocketLaunchIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography variant="h5" noWrap component="a" href="/#" sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
+                            KANAGIRA
+                        </Typography>
+                        <Box id="normalview-menubar" sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            <Button onClick={handleCloseNavMenu} component="a" href="/#" sx={{ my: 2, color: 'white', display: 'block' }}>
+                                About
+                            </Button>
+                            <Button onClick={handleCloseNavMenu} component="a" href="/#" sx={{ my: 2, color: 'white', display: 'block' }}>
+                                Contact Us
+                            </Button>
+                            <Button onClick={handleCloseNavMenu} component="a" href="/#" sx={{ my: 2, color: 'white', display: 'block' }}>
+                                FAQ
+                            </Button>
+                        </Box>
+                        <Box id="account-icon-dropdown-menu" sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar src="/broken-image.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top', horizontal: 'right', }}
+                                keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right', }} open={Boolean(anchorElUser)} onClose={handleCloseUserMenu}>
+                                <MenuItem component="a" href="/boards" onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">Dashboard</Typography>
+                                </MenuItem>
+                                <MenuItem component="a" href="#" onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">My Issues</Typography>
+                                </MenuItem>
+                                <MenuItem component="a" href="#" onClick={handleClick}>
+                                    <Typography textAlign="center">Logout</Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        );
+    }
+    else if (!token) {
+        return (
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <RocketLaunchIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography variant="h6" noWrap component="a" href="/" sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
+                            KANAGIRA
+                        </Typography>
+                        <Box id="mobile-responsive-hamburger-menu" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left', }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' }, }}>
+                                <MenuItem id="menu-appbar" component="a" href="#" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">About</Typography>
+                                </MenuItem>
+                                <MenuItem id="menu-appbar" component="a" href="#" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">Contact Us</Typography>
+                                </MenuItem>
+                                <MenuItem id="menu-appbar" component="a" href="#" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">FAQ</Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+                        <RocketLaunchIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography variant="h5" noWrap component="a" href="/#" sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
+                            KANAGIRA
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            <Button onClick={handleCloseNavMenu} component="a" href="/#" sx={{ my: 2, color: 'white', display: 'block' }}>
+                                About
+                            </Button>
+                            <Button onClick={handleCloseNavMenu} component="a" href="/#" sx={{ my: 2, color: 'white', display: 'block' }}>
+                                Contact Us
+                            </Button>
+                            <Button onClick={handleCloseNavMenu} component="a" href="/#" sx={{ my: 2, color: 'white', display: 'block' }}>
+                                FAQ
+                            </Button>
+                        </Box>
+                        <Box id="account-icon-dropdown-menu" sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar src="/broken-image.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top', horizontal: 'right', }}
+                                keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right', }} open={Boolean(anchorElUser)} onClose={handleCloseUserMenu}>
+                                <MenuItem component="a" href="/login" onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">Login</Typography>
+                                </MenuItem>
+                                <MenuItem component="a" href="/signup" onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">Sign Up</Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        );
+    }
 }
