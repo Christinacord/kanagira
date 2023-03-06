ce56930d6d75815b55e26498c9d04a5db508bcf7
Ryan <ryan2paschen@gmail.com>
2023-03-06 15:10:21 -0500

    Board form rehauled

--- ghi/src/BoardForm.js
+++ ghi/src/BoardForm.js
@@ -1,71 +1,143 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 import { useToken } from "./auth.js";
-// import { Button } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function BoardForm() {
-  const navigate = useNavigate();
-  const [name, setName] = React.useState("");
-  const { token } = useToken();
-  if (!token) {
-    return <div>Please Log In</div>;
-  }
+export default function SignupForm() {
+  const { token, signup } = useToken();
 
-  const handleNameChange = (e) => {
+  const [full_name, setFull_name] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleFull_nameChange = (e) => {
     const value = e.target.value;
-    setName(value);
+    setFull_name(value);
+  };
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+  };
+
+  const handleUsernameChange = (e) => {
+    const value = e.target.value;
+    setUsername(value);
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = {};
-    data.name = name;
-
-    const boardsUrl = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/boards`;
-    const fetchConfig = {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/json",
-      },
-    };
-
-    const response = await fetch(boardsUrl, fetchConfig);
-    if (response.ok) {
-      const entry = await response.json();
-      // navigate("/");
-    }
+    signup(full_name, username, email, password);
   };
 
+  const theme = createTheme();
+
   return (
-    <div className="login-page">
-      <div className="login-form">
-        <form onSubmit={handleSubmit} id="login-form">
-          <div className="row">
-            <div className="form">
-              <input
-                onChange={handleNameChange}
-                required
-                placeholder="name"
-                type="text"
-                id="name"
-                name="name"
-                className="form"
-              />
-              <label htmlFor="name">Name</label>
-            </div>
-          </div>
-          <button variant="outlined" size="large">
-            Create Board
-          </button>
-        </form>
-      </div>
-      <div className="kanagiraism">
-        <h2>"No more excuses, Kanagira has your back."</h2>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="full_name"
+                  required
+                  fullWidth
+                  id="full_name"
+                  label="Full Name"
+                  autoFocus
+                  value={full_name}
+                  onChange={handleFull_nameChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
-
-export default BoardForm;
