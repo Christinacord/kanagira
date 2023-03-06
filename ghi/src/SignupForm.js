25bfbd289aa4bb8aa3adafe5ac3bafb9a05d24b4
Ryan <ryan2paschen@gmail.com>
2023-03-05 23:49:47 -0500

    Signup form complete

--- ghi/src/SignupForm.js
+++ ghi/src/SignupForm.js
@@ -1,22 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 import { useToken } from "./auth.js";
-// import { Button } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-//handle change necessary?
-//form input types?
-//classnames for form - material ui?
-//The REACT_APP_ variable that you'll use may be different.
-//Pay attention to the url value for the specific service you're using.
-
-function SignupComponent() {
+export default function SignupForm() {
   const { token, signup } = useToken();
 
   const [full_name, setFull_name] = React.useState("");
   const [username, setUsername] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-
 
   const handleFull_nameChange = (e) => {
     const value = e.target.value;
@@ -43,78 +47,97 @@ function SignupComponent() {
     signup(full_name, username, email, password);
   };
 
+  const theme = createTheme();
+
   return (
-    <div className="signup-page">
-      <div className="signup-form">
-        <form onSubmit={handleSubmit} id="signup-form">
-          <div className="row">
-            <div className="form">
-              <input
-                onChange={handleFull_nameChange}
-                required
-                placeholder="full_name"
-                type="text"
-                id="full_name"
-                name="full_name"
-                className="form"
-              />
-              <label htmlFor="full_name">Full Name</label>
-            </div>
-            <div className="form">
-              <input
-                onChange={handleUsernameChange}
-                required
-                placeholder="username"
-                type="text"
-                id="username"
-                name="username"
-                className="form"
-              />
-              <label htmlFor="username">Username</label>
-            </div>
-            <div className="form">
-              <input
-                onChange={handleEmailChange}
-                required
-                placeholder="Email"
-                type="text"
-                id="Email"
-                name="Email"
-                className="form"
-              />
-              <label htmlFor="Email">Email</label>
-            </div>
-            <div className="form">
-              <input
-                onChange={handlePasswordChange}
-                required
-                placeholder="password"
-                type="password"
-                id="password"
-                name="password"
-                className="form"
-              />
-              <label htmlFor="password">Password</label>
-            </div>
-          </div>
-          <button variant="outlined" size="large">
-            Sign Up
-          </button>
-        </form>
-      </div>
-      {/* <div className="signup-link">
-        <h3 className="login-signup-link">Don't have an account?</h3>
-        <Link to="/signup">
-          <Button variant="outlined" size="large">
-            Sign Up
-          </Button>
-        </Link>
-      </div> */}
-      <div className="kanagiraism">
-        <h2>"Kanagira, because who wants to remember tasks on their own?"</h2>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="full_name"
+                  required
+                  fullWidth
+                  id="full_name"
+                  label="Full Name"
+                  autoFocus
+                  value={full_name}
+                  onChange={handleFull_nameChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
-
-export default SignupComponent;
